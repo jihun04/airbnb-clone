b163f02fd40ce0d6fe5796efc23a7e25fcdcd2e7
jihun04 <yujihun20060217@gmail.com>
2021-08-06 20:42:20 +0900

    Room Model part One

--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ class TimeStanpedModel(models.Model):
 
     """Time Stamped Model"""
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
