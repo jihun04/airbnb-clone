4f77ea0720464ee8a8cd309fee20324ee43c2990
jihun04 <yujihun20060217@gmail.com>
2021-08-07 20:16:54 +0900

    ManyToMany like a Boss

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,26 @@
 from django.db import models
+from django.db.models.fields import CharField
 from django_countries.fields import CountryField
 from core import models as core_models
 from users import models as user_models
+
+
+class AbstractItem(core_models.TimeStanpedModel):
+
+    """Abstract Item"""
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    pass
 
 
 class Room(core_models.TimeStanpedModel):
@@ -22,3 +41,7 @@ class Room(core_models.TimeStanpedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ManyToManyField(RoomType, blank=True)
+
+    def __str__(self):
+        return self.name
